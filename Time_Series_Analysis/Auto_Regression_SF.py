3a9271d5407034c314ea482165e90a5e7329e679
Mukesh.K.A <168621970+Peterpaker7@users.noreply.github.com>
2026-08-02 20:42:48 +0530

    Auto_Regression_TSA & SF_Function_of_TSA

---
+++ Time_Series_Analysis/Auto_Regression_SF.py
@@ -0,0 +1,24 @@
+class sf:
+    @staticmethod
+    def conversion(y_train,stk_data):
+        import pandas as pd
+        Actual_y_train=pd.DataFrame(index=range(len(y_train)),columns=stk_data.columns)
+        for i in range(len(y_train)):
+            Actual_y_train.iloc[i]=y_train[i]
+        return Actual_y_train
+
+    @staticmethod
+    def rmsemape(y_test,predicted_stock_price_test_ori):
+        from sklearn.metrics import mean_squared_error
+        print("RMSE-Testset : ",mean_squared_error(y_test,predicted_stock_price_test_ori,squared=False))
+        from sklearn.metrics import mean_absolute_percentage_error
+        print('maPe-testset : ',mean_absolute_percentage_error(y_test,predicted_stock_price_test_ori))
+
+    @staticmethod
+    def conversionSingle(y_train,stk_data):
+        import pandas as pd
+        Actual_y_train=pd.DataFrame(index=range(len(y_train)),columns=stk_data)
+        for i in range(len(y_train)):
+            Actual_y_train.iloc[i]=y_train[i]
+        return Actual_y_train
+        
